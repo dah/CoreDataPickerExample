e0ac97daccad8d4dbe7674056d4fe423b38c2164
Dan Hancu <dan@hancu.org>
2021-04-08 11:26:35 +0100

    .

--- CoreDataPickerExample/ContentView.swift
+++ CoreDataPickerExample/ContentView.swift
@@ -65,8 +65,6 @@ struct ContentView: View {
                 try viewContext.save()
                 selection = newItem //This automatically changes your selection when you add a new item.
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -80,8 +78,6 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
